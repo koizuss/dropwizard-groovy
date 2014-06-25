ed37bfcfa9281b04a845f0e6c48c1fb5791c82c6
koizuss <koizuss.apps@gmail.com>
2014-06-25 09:20:58 +0900

    use jaxrs annotation

--- src/main/groovy/GreetResource.groovy
+++ src/main/groovy/GreetResource.groovy
@@ -1,11 +1,22 @@
 import javax.ws.rs.*
+import javax.ws.rs.core.Context
+import javax.servlet.http.HttpServletRequest
 
 @Path('/greet')
 class GreetResouce {
     def template
 
+    @Context
+    HttpServletRequest req
+
     @GET
     def greet() {
+        req.headerNames.each {
+            println "$it: ${req.getHeaders(it).collect()}"
+        }
+        req.parameterNames.each {
+            println "$it: ${req.getParameterValues(it)}"
+        }
         return String.format(template, 'dropwizard')
     }
 }
