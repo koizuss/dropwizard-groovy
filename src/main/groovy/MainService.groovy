981b21ca74eb68c09313fe988619fe10ade33b88
koizuss <koizuss.apps@gmail.com>
2014-06-25 12:59:32 +0900

    show static contents

--- src/main/groovy/MainService.groovy
+++ src/main/groovy/MainService.groovy
@@ -2,13 +2,17 @@ import io.dropwizard.Application
 import io.dropwizard.Configuration
 import io.dropwizard.setup.Bootstrap
 import io.dropwizard.setup.Environment
+import io.dropwizard.assets.AssetsBundle
 
 class MainService extends Application<Conf> {
     static main(args) {
         new MainService().run(args)
     }
 
-    void initialize(Bootstrap bootstrap) {}
+    void initialize(Bootstrap bootstrap) {
+        bootstrap.addBundle(new AssetsBundle())
+    }
+
     void run(Conf conf, Environment env) {
         final greet = new GreetResouce(template: conf.template)
         env.jersey().register greet
